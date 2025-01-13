31a7ef2216a5990005aa470c8bdf091217ad040a
Gabrielly de Oliveira Sales Almeida <gabrielly.almeida@ourobranco.mg.gov.br>
2025-01-13 11:43:56 -0300

    Exercício usando estruturas de repetições, while e do while

---
+++ exercicios/aula_13/ambiente.js
@@ -0,0 +1,23 @@
+//Estrutura de repetição com teste lógico no início:
+var c = 1
+while (c <= 6) {
+    console.log(`Passo ${c}`)
+    c++ //c = c + 1
+}
+
+/* Estrutura de repetição com teste lógico no final:
+    var n = 1
+    do {
+        console.log(`Passo ${n}`)
+        n++
+    } while (n <= 6)
+*/
+
+/* Mesmo modelo sem estrutura de repetição
+    console.log('Passo 1')
+    console.log('Passo 2')
+    console.log('Passo 3')
+    console.log('Passo 4')
+    console.log('Passo 5')
+    console.log('Passo 6')
+*/
