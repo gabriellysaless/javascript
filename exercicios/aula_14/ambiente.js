3b9bf604d26c25b8da1a4f71d46e117eaffafb3a
Gabrielly de Oliveira Sales Almeida <gabrielly.almeida@ourobranco.mg.gov.br>
2025-01-13 14:30:56 -0300

    Exercício criado sobre repetições com o for

---
+++ exercicios/aula_14/ambiente.js
@@ -0,0 +1,13 @@
+/*
+    var n = 1
+    while (n <= 5) {
+        console.log(n)
+        n++
+    }
+*/
+
+console.log('Vai começar...')
+for (var n = 1; n <= 5; n++) {
+    console.log(n)
+}
+console.log('Fim')
