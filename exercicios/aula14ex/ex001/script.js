4ab188ddd3d800204297a0103c44c3063d23b168
Gabrielly de Oliveira Sales Almeida <gabrielly.almeida@ourobranco.mg.gov.br>
2025-01-17 11:14:57 -0300

    Exercício contador inteligente terminado

--- exercicios/aula14ex/ex001/script.js
+++ exercicios/aula14ex/ex001/script.js
@@ -1,8 +1,36 @@
-//testes
 function contar() {
     let inicio = document.getElementById('istart')
     let fim = document.getElementById('iend')
     let passo = document.getElementById('istep')
     let res = document.getElementById('resul')
-    res.innerHTML = `${Number(fim.value) + Number(passo.value)}`
+
+    if (inicio.value === "" || fim.value === "" || passo.value === ""){
+        window.alert('Por favor, preencha todos os campos!')
+    } else {
+        let i = Number(inicio.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
+    
+    if (p <= 0) {
+        window.alert('Passo inválido! Considerando PASSO = 1') 
+        p = 1
+    }
+
+    res.innerHTML = 'Contando... <br>'
+
+    if (i < f) {
+        while (i <= f) {
+            res.innerHTML += `${i} &#x1F449`
+            i = `${i + p}`
+            i = Number(i)
+        }
+    } else {
+        while (i >= f) {
+            res.innerHTML += `${i} &#x1F449`
+            i = `${i - p}`
+            i = Number(i)
+        }
+    }
+    res.innerHTML += '&#x1F3C1'
+    }
 }
