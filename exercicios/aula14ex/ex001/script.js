a3f61e27b096d8d261ec593ec772e73e4b19e8f6
Gabrielly de Oliveira Sales Almeida <gabrielly.almeida@ourobranco.mg.gov.br>
2025-01-17 15:09:31 -0300

    Acrécimo de comentários, mudança de while para for, e acrécimo de uma variável para contar

--- exercicios/aula14ex/ex001/script.js
+++ exercicios/aula14ex/ex001/script.js
@@ -4,12 +4,13 @@ function contar() {
     let passo = document.getElementById('istep')
     let res = document.getElementById('resul')
 
+    // Ou if (inicio.value.length == 0 ...) length: comprimento (quantas letras tem)
     if (inicio.value === "" || fim.value === "" || passo.value === ""){
         window.alert('Por favor, preencha todos os campos!')
     } else {
         let i = Number(inicio.value)
-    let f = Number(fim.value)
-    let p = Number(passo.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
     
     if (p <= 0) {
         window.alert('Passo inválido! Considerando PASSO = 1') 
@@ -17,18 +18,16 @@ function contar() {
     }
 
     res.innerHTML = 'Contando... <br>'
-
+    /* Explicações para entendimento:
+        Abaixo coloquei uma variável para contar. Essa é uma boa prática pq guarda o valor inicial inserido (i) e evita confusão no código
+    */
     if (i < f) {
-        while (i <= f) {
-            res.innerHTML += `${i} &#x1F449`
-            i = `${i + p}`
-            i = Number(i)
+        for (let cont = i; cont <= f; cont += p) {
+            res.innerHTML += `${cont} &#x1F449`
         }
     } else {
-        while (i >= f) {
-            res.innerHTML += `${i} &#x1F449`
-            i = `${i - p}`
-            i = Number(i)
+        for (let cont = i; cont >= f; cont -= p) {
+            res.innerHTML += `${cont} &#x1F449`
         }
     }
     res.innerHTML += '&#x1F3C1'
