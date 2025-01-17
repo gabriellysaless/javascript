a8f3c19df52786a6888424d7cde8241805a6b39d
Gabrielly de Oliveira Sales Almeida <gabrielly.almeida@ourobranco.mg.gov.br>
2025-01-17 15:47:39 -0300

    Atualização do exercício. Função limpar tela, troca de while por for, e acrécimo do value para cada option do select

--- exercicios/aula14ex/ex002/script.js
+++ exercicios/aula14ex/ex002/script.js
@@ -1,17 +1,18 @@
 function clicou(){
-    var numb = document.getElementById('inum')
-    var resp = document.getElementById('itxt')
-    if (numb.value == '') {
+    let numb = document.getElementById('inum')
+    let resp = document.getElementById('itxt')
+    if (numb.value.length == 0) {
         window.alert('[Erro] Digite um número para obter a tabuada')
     } else {
-        var n = Number(numb.value)
-        var c = 1
-        var s = 0
-        while (c<=10) {
-            var item = document.createElement('option') // cria o elemento option
+        let n = Number(numb.value)
+        let c = 1
+        resp.innerHTML = '' // Para limpar a tela quando for mostrar a tabuada de outro número
+
+        for (let c = 1; c <= 10; c++) {
+            let item = document.createElement('option') // cria o elemento option
             item.text = `${n} x ${c} = ${n*c}` // o q vai ficar escrito dentro do option
+            item.value = `tab${c}` // para dar o value, que é útil em outras linguagem como PHP
             resp.appendChild(item) // adiciona o elemento filho (item) dentro da resp (select no HTML)
-            c++
         }
     }
 }
