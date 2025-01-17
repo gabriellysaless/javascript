583c27ca6053cae9c260d7be1c1972b0f4e3bd43
Gabrielly de Oliveira Sales Almeida <gabrielly.almeida@ourobranco.mg.gov.br>
2025-01-17 15:26:37 -0300

    Mudei a formatação do emoji para JavaScript

--- exercicios/aula14ex/ex001/script.js
+++ exercicios/aula14ex/ex001/script.js
@@ -7,29 +7,30 @@ function contar() {
     // Ou if (inicio.value.length == 0 ...) length: comprimento (quantas letras tem)
     if (inicio.value === "" || fim.value === "" || passo.value === ""){
         window.alert('Por favor, preencha todos os campos!')
+        res.innerHTML = 'Preencha os campos acima!'
     } else {
         let i = Number(inicio.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        res.innerHTML = 'Contando... <br>'
     
-    if (p <= 0) {
-        window.alert('Passo inválido! Considerando PASSO = 1') 
-        p = 1
-    }
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO = 1') 
+            p = 1
+        }
 
-    res.innerHTML = 'Contando... <br>'
-    /* Explicações para entendimento:
-        Abaixo coloquei uma variável para contar. Essa é uma boa prática pq guarda o valor inicial inserido (i) e evita confusão no código
-    */
-    if (i < f) {
-        for (let cont = i; cont <= f; cont += p) {
-            res.innerHTML += `${cont} &#x1F449`
+        /* Explicações para entendimento:
+            Abaixo coloquei uma variável para contar. Essa é uma boa prática pq guarda o valor inicial inserido (i) e evita confusão no código
+        */
+        if (i < f) {
+            for (let cont = i; cont <= f; cont += p) {
+                res.innerHTML += `${cont} \u{1F449}` //&#x1F449
+            }
+        } else {
+            for (let cont = i; cont >= f; cont -= p) {
+                res.innerHTML += `${cont} \u{1F449}` //&#x1F449
+            }
         }
-    } else {
-        for (let cont = i; cont >= f; cont -= p) {
-            res.innerHTML += `${cont} &#x1F449`
+        res.innerHTML += `\u{1F3C1}` //&#x1F3C1
         }
-    }
-    res.innerHTML += '&#x1F3C1'
-    }
 }
