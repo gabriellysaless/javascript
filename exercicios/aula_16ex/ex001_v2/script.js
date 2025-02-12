34ab6a553cf69b4e1f6292e8ca346c41eb2318e9
Gabrielly de Oliveira Sales Almeida <gabrielly.almeida@ourobranco.mg.gov.br>
2025-02-12 08:46:32 -0300

    Outra forma de fazer o exercício criado

--- exercicios/aula_16ex/ex001_v2/script.js
+++ exercicios/aula_16ex/ex001_v2/script.js
@@ -12,7 +12,7 @@ function isNum(n) {
 }
 
 function inList(n, l) {
-    if (l.indexOf(Number(n)) != -1) {
+    if (l.indexOf(Number(n)) != -1) { //-1 significa que não está no array
         return true
     } else {
         return false
@@ -21,8 +21,43 @@ function inList(n, l) {
 
 function adicionar() {
     if (isNum(num.value) && !inList(num.value, valores)) {
-        window.alert ('Tudo ok!')
+        valores.push(Number(num.value))
+        let texto = document.createElement('option')
+        texto.innerHTML = `Valor ${num.value} adicionado`
+        resp.appendChild(texto)
     } else {
         window.alert ('Número inválido ou já selecionado.')
     }
+    res.innerHTML = ``
+    num.value = ''
+    num.focus()
+}
+
+function finalizar() {
+    if (valores.length === 0) {
+        window.alert('Digite um valor para obter o resultado')
+    } else {
+        let tot = valores.length
+        let maior = valores[0]
+        let menor = valores [0]
+        let soma = 0
+        for(let pos in valores){ // "Para cada posição dentro de valores faça:"
+            soma += valores[pos]
+            if (valores[pos] > maior) {
+                maior = valores[pos]
+            }
+            if (valores[pos] < menor) {
+                menor = valores[pos]
+            }
+        }
+        media = soma / valores.length
+        res.innerHTML = `
+        <p>Ao todo temos ${tot} números cadastrados.</p>
+        <p>O maior valor informado foi ${maior}.</p>
+        <p>O menor valor informado foi ${menor}.</p>
+        <p>Somando todos os valores, temos ${soma}.</p>
+        <p>A média dos valores digitados é ${media.toFixed(2)}.</p>
+        `
+    }
+    // quantidade de numeros, maior e menor valor, soma, media
 }
